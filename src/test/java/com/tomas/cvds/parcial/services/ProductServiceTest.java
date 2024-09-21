2729a901e996b2c9592e66cc8e9de32bd541704b
t-hash06 <tomasfelipe.p@gmail.com>
2024-09-21 12:37:31 -0500

    test(products): add some ProductService test, 100% coverage on service module has been reached

--- src/test/java/com/tomas/cvds/parcial/services/ProductServiceTest.java
+++ src/test/java/com/tomas/cvds/parcial/services/ProductServiceTest.java
@@ -41,9 +41,84 @@ public class ProductServiceTest {
         ProductModel createdProduct = productService.getProductByName("Product");
 
         assertEquals(10, (int) createdProduct.getStock());
-        productService.updateProductStock("Product", 20);
+        productService.updateProductStock("Product", 4);
 
         ProductModel updatedProduct = productService.getProductByName("Product");
-        assertEquals(20, (int) updatedProduct.getStock());
+        assertEquals(4, (int) updatedProduct.getStock());
+    }
+
+    @Test
+    public void shouldDeleteAllProducts() throws ProductException.ProductConflictException {
+        ProductModel model1 = new ProductModel();
+        ProductModel model2 = new ProductModel();
+
+        model1.setName("Product").setPrice(1000).setCategory("Category").setStock(10);
+        model2.setName("Product2").setPrice(1000).setCategory("Category").setStock(10);
+
+        productService.createProduct(model1);
+        productService.createProduct(model2);
+
+        assertTrue(productService.getAllProducts().contains(model1));
+        assertTrue(productService.getAllProducts().contains(model2));
+        productService.deleteAllProducts();
+        assertTrue(productService.getAllProducts().isEmpty());
+    }
+
+    @Test
+    public void shouldFindAProductByName() throws ProductException.ProductConflictException, ProductException.ProductNotFoundException {
+        ProductModel model = new ProductModel();
+
+        model.setName("Product").setPrice(1000).setCategory("Category").setStock(10);
+        productService.createProduct(model);
+
+        ProductModel createdProduct = productService.getProductByName("Product");
+
+        assertEquals(model, createdProduct);
+    }
+
+    @Test
+    public void shouldErrorIfProductNameAlreadyExists() throws ProductException.ProductConflictException {
+        ProductModel model = new ProductModel();
+
+        model.setName("Product").setPrice(1000).setCategory("Category").setStock(10);
+        productService.createProduct(model);
+
+        try {
+            productService.createProduct(model);
+        } catch (ProductException.ProductConflictException e) {
+            assertEquals("Product already exists", e.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldErrorIfProductDoesNotExistOnGetProduct() {
+        try {
+            productService.getProductByName("Product");
+        } catch (ProductException.ProductNotFoundException e) {
+            assertEquals("Product not found", e.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldErrorIfProductDoesNotExistOnUpdateProduct() {
+        try {
+            productService.updateProductStock("Product", 10);
+        } catch (ProductException.ProductNotFoundException | ProductException.ProductStockNegativeException e) {
+            assertEquals("Product not found", e.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldErrorIfProductStockIsNegative() throws ProductException.ProductConflictException {
+        ProductModel model = new ProductModel();
+
+        model.setName("Product").setPrice(1000).setCategory("Category").setStock(10);
+        productService.createProduct(model);
+
+        try {
+            productService.updateProductStock("Product", -1);
+        } catch (ProductException.ProductNotFoundException | ProductException.ProductStockNegativeException e) {
+            assertEquals("Product stock cannot be negative", e.getMessage());
+        }
     }
 }
